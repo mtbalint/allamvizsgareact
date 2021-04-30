5ed5ab7d8545b094b19b4386f5fe9be5a64615b9
mtbalint <szimbol.hu@gmail.com>
2021-04-30 12:36:57 +0200

    players shows

--- src/components/Team.js
+++ src/components/Team.js
@@ -7,21 +7,24 @@ function Team(props) {
   function clickHandler() {
     setShow(!show);
   }
+  let teamplayers = props.item.franchisePlayers;
 
   return (
     <div className="team">
       <div>
-        <p>{props.item.name}</p>
+        <p>
+          <strong>{props.item.name}</strong>
+        </p>
         <p>{props.item.stadium}</p>
 
         {show ? (
           <>
-            <p>
-              <button onClick={clickHandler}>Show less</button>
-            </p>
-            <div>
-              {props.item.stadium} ({props.item.stadium})<br />
-            </div>
+            <button onClick={clickHandler}>Show less</button>
+            {teamplayers.map((player, index) => (
+              <div>
+                <p>{player.name}</p>
+              </div>
+            ))}
           </>
         ) : (
           <button onClick={clickHandler}>show more</button>
